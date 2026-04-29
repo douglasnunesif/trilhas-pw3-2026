bf02451459a14697fcc0edf22302fe88d22055a5
Douglas Nunes <douglas.nunes@ifsuldeminas.edu.br>
2026-04-29 15:00:11 -0300

    refatorando o css do login

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const loginRoutes = require("./src/routes/loginRoutes");
+const userRoutes = require("./src/routes/userRoutes");
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // Rotas
 app.use("/api/login", loginRoutes);
+app.use("/api/users", userRoutes);
 
 // Inicia o servidor
 app.listen(PORT, () => {
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,17 +1,31 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "ifsuldeminas",
-    database: "ecommerce"
-})
+  host: "localhost",
+  user: "root",
+  password: "ifsuldeminas",
+  database: "ecommerce",
+});
 
-exports.findByEmail = (email,callback) =>{
-    const sql = "SELECT * from users WHERE email = ?";
-    db.query(sql, [email], (err, result)=> {
-        if(err) throw err
-        callback(result[0]);
-    })
+exports.createUser = (user, callback) => {
+  const sql =
+    "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
+  db.query(sql, [user.name, user.email, user.password, user.role], callback);
+};
 
-}
+exports.update = (email, user, callback) => {
+  const sql = "UPDATE users SET name=?, email=?, password=? WHERE email=?";
+  db.query(sql, [user.name, user.email, user.password, email], callback);
+};
+
+exports.delete = (email, callback) => {
+  db.query("DELETE FROM users WHERE email=?", [email], callback);
+};
+
+exports.findByEmail = (email, callback) => {
+  const sql = "SELECT * from users WHERE email = ?";
+  db.query(sql, [email], (err, result) => {
+    if (err) throw err;
+    callback(result[0]);
+  });
+};
